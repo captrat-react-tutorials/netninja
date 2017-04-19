661dabf72351c634d7c08bfe5741634cd03e93c0
captrat <jpatricklogan@gmail.comgit config user.name>
2017-04-19 14:45:41 -0600

    feat(DOM injection)Add 'cheese' object onto .html

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-alert('it works!');
+//create Component
+var ToDoComponent = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <p><strong>Cheese name: </strong> {this.props.cheese.name}</p>
+        <p><strong>Cheese smellFactor: </strong> {this.props.cheese.smellFactor}</p>
+        <p><strong>Cheese price: </strong> {this.props.cheese.price}</p>
+
+
+      </div>
+    );
+  }
+});
+
+var myCheese = {
+  name: "Camembert",
+  smellFactor: "Extreme pong",
+  price: "3.50"
+}
+
+//put Component into HTML page
+ReactDOM.render(<ToDoComponent mssg="I like cheese" cheese={myCheese} />, document.getElementById('todo-wrapper'));
