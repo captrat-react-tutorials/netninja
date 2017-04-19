d7875d759ba319dadf54cb9069c39b883d2facc0
captrat <jpatricklogan@gmail.comgit config user.name>
2017-04-19 16:07:41 -0600

    Cleaned up code to #5.5

--- src/app/index.js
+++ src/app/index.js
@@ -3,24 +3,33 @@ var ReactDOM = require('react-dom');
 
 //create Component
 var ToDoComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: ['wash up', 'eat some cheese', 'take a nap'],
+      age: 30
+    }
+  },
+
   render: function(){
+    var ager = setTimeout(function(){
+      this.setState({
+        age: 35
+      });
+    }.bind(this), 5000);
+
     return(
-      <div>
-        <p><strong>Cheese name: </strong> {this.props.cheese.name}</p>
-        <p><strong>Cheese smellFactor: </strong> {this.props.cheese.smellFactor}</p>
-        <p><strong>Cheese price: </strong> {this.props.cheese.price}</p>
-
-
+      <div id="todo-list">
+        <p>Comfort retards growth.</p>
+        <p>{this.state.age}</p>
+          <ul>
+            <li> {this.state.todos[0]}</li>
+            <li> {this.state.todos[1]}</li>
+            <li> {this.state.todos[2]}</li>
+          </ul>
       </div>
     );
   }
 });
 
-var myCheese = {
-  name: "Camembert",
-  smellFactor: "Extreme pong",
-  price: "3.50"
-}
-
 //put Component into HTML page
-ReactDOM.render(<ToDoComponent mssg="I like cheese" cheese={myCheese} />, document.getElementById('todo-wrapper'));
+ReactDOM.render(<ToDoComponent />, document.getElementById('todo-wrapper'));
